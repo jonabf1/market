9794814b6d11e91e76475be64a34ffdd71c2016a
Baki <jonabf1@gmail.com>
2020-11-06 21:26:31 -0300

    update/inclusão do FluentValidator

--- src/main/java/com/api/market/business/ProductBusiness.java
+++ src/main/java/com/api/market/business/ProductBusiness.java
@@ -2,10 +2,16 @@ package com.api.market.business;
 
 import com.api.market.dto.ProductDto;
 import com.api.market.entity.Product;
+import com.api.market.handler.ValidationBadRequest;
 import com.api.market.mapper.ProductMapperImpl;
 import com.api.market.repository.ProductRepository;
+import com.api.market.validator.ProductValidator;
+import javassist.tools.web.BadHttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,10 +22,16 @@ public class ProductBusiness {
     @Autowired
     ProductRepository productRepository;
 
-    public Product salvar (ProductDto product){
-      Product productSaved = productRepository.save(ProductMapperImpl.INSTANCE.mapFrom(product));
+    @Autowired
+    ProductValidator productValidator;
 
-      return productSaved;
+    public Product salvar (ProductDto product){
+      productValidator.validate(product);
+
+      Product productMapped = ProductMapperImpl.INSTANCE.mapFrom(product);
+//      Product productSaved = productRepository.save(productMapped);
+
+      return productMapped;
     }
 
     public Optional<Product> recuperarPorId(Integer id){
---
+++ src/main/java/com/api/market/handler/ValidationBadRequest.java
@@ -0,0 +1,11 @@
+package com.api.market.handler;
+
+import javassist.tools.web.BadHttpRequest;
+
+public class ValidationBadRequest extends BadHttpRequest {
+
+    public ValidationBadRequest(){
+
+    }
+
+}
